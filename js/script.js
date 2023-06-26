0189473c9277507744a9227888a44f7aef5bfddd
Monali Valunj <monavalunj96@gmail.com>
2023-06-25 17:31:21 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,142 @@
+// //STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryfunction;
+
+
+// //STEP 2
+// let num = 1 + 2;
+// let name = "John Smith";
+// let value = true;
+// let val1 = null;
+
+
+// //STEP 3
+// let myName = "Monali";
+// let age = 30;
+// let msg;
+// msg = myName + " is " + age + " years old."
+// console.log(msg);
+// let a = 3;
+// let b = 5;
+// let c = 8;
+// let answer = a +b * c;
+// console.log("answer is " + answer)
+
+
+// //STEP 4
+// let firstName, lastName, address, city, state, nZipCode;
+// let yourAge, referralSource, mayWeContactYou;
+
+// //STEP 5
+// let firstName, city, zipCode;
+// firstName = "MONALI";
+// city = "San Diego";
+// zipCode = 123456;
+
+// let firstName;
+// let city;
+// let zipCode;
+
+// let firstName = "Monali";
+// let city = "San Diego";
+// let zipCode = 234567;
+
+
+// //STEP 6
+// let var1 = 23 + "Three";
+// console.log(var1);
+
+// //STEP 7
+// let var1 = true + "one";
+// console.log(var1);
+
+// let var2 = 33 + true;
+// console.log(var2);
+
+// //STEP 8
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+// console.log(someString);
+
+// //STEP 9
+// let number1 = null;
+// console.log(number1);
+// let number2 = undefined;
+// console.log(number2);
+
+// // STEP10
+// let str = "name";
+// console.log(typeof str);
+
+// let num = 1;
+// console.log(typeof num);
+
+// let val = true;
+// console.log(typeof val);
+// console.log(typeof true);
+
+// console.log(typeof undefined);
+// let obj = {};
+// console.log(typeof obj);
+
+
+// //STEP 11
+// window.alert("Hello "+ "Monali Valunj, " +"welcome to the JavaScript class!");
+
+
+// //STEP 12
+// let myName = "Monali Valunj";
+// window.alert("Hello "+ myName +", " +"welcome to the JavaScript class!");
+
+
+
+// //STEP 13
+// let myName = "Monali Valunj";
+// let course = "Javascript"
+// window.alert("Hello "+ myName +", " +"welcome to the " + course + " class!");
+
+
+// //STEP 14
+// let myName = "Monali Valunj";
+// let course = "Javascript"
+// window.alert("Hello "+ myName +"," +"\nwelcome to the " + course + " class!");
+
+
+// //STEP 15
+// let myName = prompt("Enter your name: ");
+// let course = "Javascript"
+// window.alert("Hello "+ myName +"," +"\nwelcome to the " + course + " class!");
+
+
+// //STEP 16
+// let myName = prompt("Enter your name: ");
+// let course = prompt("Which class are you taking: ");
+// window.alert("Hello "+ myName +"," +"\nwelcome to the " + course + " class!");
+
+
+// //STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x+y);
+
+// //STEP 18 
+// let x = 20;
+// console.log(x+=20);
+
+// //STEP19
+// let x = 20;
+// console.log(x*=5);
+
+// //STEP 20
+// let x = 20%3;
+// console.log(x/=1);
+
+// // STEP 21
+// let a =3, b =4;
+// console.log(a<b && a!=b);
+
+// //STEP 22
+// let a =3, b =4;
+// console.log(a==b || a>=b);
